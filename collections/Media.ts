a82b5ba2d56720de115f8ef8061304a67eed0dd1
Philani Ncube <ncbphi001@gmail.com>
2025-04-21 15:12:56 +0200

    static folders

--- collections/Media.ts
+++ collections/Media.ts
@@ -3,7 +3,7 @@ import type { CollectionConfig } from 'payload'
 export const Media: CollectionConfig = {
   slug: 'media',
   upload: {
-    staticDir: 'media',
+    staticDir: './public/images/media',
     imageSizes: [
       { name: 'thumbnail', width: 400, height: 300, position: 'center' },
       { name: 'card', width: 768, height: 1024, position: 'center' },
