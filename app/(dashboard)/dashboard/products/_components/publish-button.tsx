bf7c3576750d4f00d467102e4374cd50b99b0881
Philani Ncube <ncbphi001@gmail.com>
2025-04-26 15:34:12 +0200

    feat: add PublishButton component for publishing Printify products; implement publish functionality

---
+++ app/(dashboard)/dashboard/products/_components/publish-button.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Button } from '@/components/ui/button';
+import { publishPrintifyProduct } from '@/utils/actions/products';
+import { CircleDashed } from 'lucide-react';
+import React, {  useTransition } from 'react'
+
+const PublishButton = ({ productId }: { productId: string }) => {
+
+  const [isPending, startTransition] = useTransition()
+
+  const publishProduct = async () => {
+    const response = await publishPrintifyProduct(productId)
+    if (response.error) {
+      console.error('Error publishing product:', response.error)
+      return
+    }
+    console.log('Product published successfully:', response)
+  }
+
+
+  return (
+    <div>
+      <Button disabled={isPending} onClick={() => {
+        startTransition(() => {
+          publishProduct()
+        })
+      }} className="w-full relative">
+        {isPending && (
+          <div className="absolute inset-0 flex items-center justify-center">
+            <CircleDashed className="animate-spin" />
+          </div>
+        )}
+        Publish
+      </Button>
+    </div>
+  )
+}
+
+export default PublishButton
