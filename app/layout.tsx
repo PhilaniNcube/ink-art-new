264b110cda5a7942d7ca60237887e26cb2e91300
Philani Ncube <ncbphi001@gmail.com>
2025-07-06 00:36:52 +0200

    refactor: Remove unused script tag from RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,6 @@ import "./globals.css";
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
-      <head>
-        <script
-          crossOrigin="anonymous"
-          src="//unpkg.com/react-scan/dist/auto.global.js"
-          async
-        />
-      </head>
       <body>{children}</body>
     </html>
   );
